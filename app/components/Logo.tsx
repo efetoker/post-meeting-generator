e59a4ea4358b065ab6982801a7d8f052bf941c49
Efe TOKER <e.toker@medyatakip.com>
2025-09-20 07:41:35 +0300

    feat: unique svg logo implemented for showing it on navbar

---
+++ app/components/Logo.tsx
@@ -0,0 +1,27 @@
+// app/components/Logo.tsx
+
+export function Logo() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="h-6 w-6"
+    >
+      <path d="M12 12v3" />
+      <path d="M18 6v1" />
+      <path d="M18 10v1" />
+      <path d="M18 15v1" />
+      <path d="M12 6V5a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4a2 2 0 0 1-2-2v-1" />
+      <path d="M6 3v1" />
+      <path d="M6 7v1" />
+      <path d="M6 12v1" />
+      <path d="M6 17v1" />
+      <path d="M12 20v-1a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2Z" />
+    </svg>
+  );
+}
