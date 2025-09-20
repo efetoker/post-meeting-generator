0d87be21ee202f1eea0b72cf44010f1d972442f3
Efe TOKER <e.toker@medyatakip.com>
2025-09-20 14:56:17 +0300

    refactor: spacing issue fixed

--- app/(protected)/past-meetings/[id]/page.tsx
+++ app/(protected)/past-meetings/[id]/page.tsx
@@ -48,30 +48,32 @@ export default function MeetingDetailPage() {
   if (isLoading) {
     return (
       <div className="container mx-auto max-w-4xl p-4 md:p-8">
-        <div className="mb-6">
+        <div className="mb-8">
           <Skeleton className="h-10 w-3/4 mb-2" />
           <Skeleton className="h-6 w-1/3" />
         </div>
-        <Card className="mb-8">
-          <CardContent className="p-6">
-            <div className="space-y-2">
-              <Skeleton className="h-4 w-full" />
-              <Skeleton className="h-4 w-5/6" />
-              <Skeleton className="h-4 w-3/4" />
-            </div>
-          </CardContent>
-        </Card>
+        <div className="space-y-4">
+          <Card>
+            <CardContent className="p-6">
+              <div className="space-y-2">
+                <Skeleton className="h-4 w-full" />
+                <Skeleton className="h-4 w-5/6" />
+                <Skeleton className="h-4 w-3/4" />
+              </div>
+            </CardContent>
+          </Card>
 
-        <Card className="mb-8">
-          <CardHeader>
-            <Skeleton className="h-7 w-1/2" />
-            <Skeleton className="h-5 w-2/3" />
-          </CardHeader>
-          <CardContent>
-            <Skeleton className="h-10 w-48 mb-4" />
-            <Skeleton className="h-40 w-full" />
-          </CardContent>
-        </Card>
+          <Card>
+            <CardHeader>
+              <Skeleton className="h-7 w-1/2" />
+              <Skeleton className="h-5 w-2/3" />
+            </CardHeader>
+            <CardContent>
+              <Skeleton className="h-10 w-48 mb-4" />
+              <Skeleton className="h-28 w-full" />
+            </CardContent>
+          </Card>
+        </div>
       </div>
     );
   }
